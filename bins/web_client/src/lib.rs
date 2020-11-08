625c83cb40cc42a0b3fec6f62e521fc2fc871034
mvlabat <mvlabat@gmail.com>
2020-11-08 18:04:52 +0200

    Render a basic 3D scene

--- bins/web_client/src/lib.rs
+++ bins/web_client/src/lib.rs
@@ -1,12 +1,17 @@
 use bevy::{
-    input::{
-        keyboard::KeyboardInput,
-        mouse::{MouseButtonInput, MouseMotion, MouseWheel},
-    },
+    input::{keyboard::KeyboardInput, mouse::MouseButtonInput},
     prelude::*,
 };
-use wasm_bindgen::prelude::*;
-// use wasm_bindgen::JsCast;
+use wasm_bindgen::{prelude::*, JsCast};
+
+#[derive(Default)]
+pub struct WindowInnerSize {
+    width: usize,
+    height: usize,
+}
+
+#[derive(Default)]
+pub struct MousePosition(Vec2);
 
 #[wasm_bindgen(start)]
 pub fn main() {
@@ -19,83 +24,102 @@ pub fn main() {
             height: 768,
             ..Default::default()
         })
+        .add_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
-        // One time greet
-        .add_startup_system(hello_wasm_system.system())
-        // Track ticks (sanity check, whether game loop is running)
-        .add_system(counter.system())
-        // .add_system(resize_canvas.system())
-        // Track input events
+        .init_resource::<WindowInnerSize>()
+        .add_system(resize_canvas.system())
+        // Startup systems,
+        .add_startup_system(basic_scene.system())
+        // Track input events.
         .init_resource::<TrackInputState>()
         .add_system(track_input_events.system())
         .run();
 }
 
-fn hello_wasm_system() {
-    log::info!("hello wasm");
+fn basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // add entities to the world
+    commands
+        // plane
+        .spawn(PbrComponents {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            ..Default::default()
+        })
+        // cube
+        .spawn(PbrComponents {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_translation(Vec3::new(0.0, 1.0, 0.0)),
+            ..Default::default()
+        })
+        // light
+        .spawn(LightComponents {
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+            ..Default::default()
+        })
+        // camera
+        .spawn(Camera3dComponents {
+            transform: Transform::from_translation(Vec3::new(-3.0, 5.0, 8.0))
+                .looking_at(Vec3::default(), Vec3::unit_y()),
+            ..Default::default()
+        });
 }
 
-fn counter(mut state: Local<CounterState>, time: Res<Time>) {
-    if state.count % 60 == 0 {
-        log::info!(
-            "tick {} @ {:?} [Δ{}]",
-            state.count,
-            time.time_since_startup(),
-            time.delta_seconds
-        );
+fn resize_canvas(mut window_inner_size: ResMut<WindowInnerSize>) {
+    let window = web_sys::window().expect("no global `window` exists");
+
+    let width = window
+        .inner_width()
+        .expect("window should have size")
+        .as_f64()
+        .unwrap() as usize;
+    let height = window
+        .inner_height()
+        .expect("window should have size")
+        .as_f64()
+        .unwrap() as usize;
+
+    if window_inner_size.width != width || window_inner_size.height != height {
+        window_inner_size.width = width;
+        window_inner_size.height = height;
+
+        let document = window.document().expect("should have a document on window");
+        let canvas = document
+            .get_elements_by_tag_name("canvas")
+            .item(0)
+            .expect("body should have a canvas")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("expected to be a canvas");
+
+        canvas
+            .style()
+            .set_css_text(&format!("width: {}px; height: {}px;", width, height));
+        canvas
+            .set_attribute("width", &width.to_string())
+            .expect("expected to set canvas width");
+        canvas
+            .set_attribute("height", &height.to_string())
+            .expect("expected to set canvas width");
     }
-    state.count += 1;
-}
-
-// fn resize_canvas() {
-//     let window = web_sys::window().expect("no global `window` exists");
-//     let document = window.document().expect("should have a document on window");
-//     let body = document.body().expect("document should have a body");
-//
-//     let canvas = document
-//         .get_elements_by_tag_name("canvas")
-//         .item(0)
-//         .expect("body should have a canvas")
-//         .dyn_into::<web_sys::HtmlCanvasElement>()
-//         .expect("expected to be a canvas");
-//
-//     canvas
-//         .style()
-//         .set_property("width", &body.client_width().to_string())
-//         .expect("expected to set canvas width");
-//     canvas
-//         .style()
-//         .set_property("height", &body.client_height().to_string())
-//         .expect("expected to set canvas height");
-//     canvas
-//         .set_attribute("width", &body.client_width().to_string())
-//         .expect("expected to set canvas width");
-//     canvas
-//         .set_attribute("height", &body.client_height().to_string())
-//         .expect("expected to set canvas height");
-// }
-
-#[derive(Default)]
-struct CounterState {
-    count: u32,
 }
 
 #[derive(Default)]
 struct TrackInputState {
     keys: EventReader<KeyboardInput>,
     cursor: EventReader<CursorMoved>,
-    motion: EventReader<MouseMotion>,
-    mousebtn: EventReader<MouseButtonInput>,
-    scroll: EventReader<MouseWheel>,
+    mouse_button: EventReader<MouseButtonInput>,
 }
 
 fn track_input_events(
     mut state: ResMut<TrackInputState>,
+    mut mouse_position: ResMut<MousePosition>,
     ev_keys: Res<Events<KeyboardInput>>,
     ev_cursor: Res<Events<CursorMoved>>,
-    ev_motion: Res<Events<MouseMotion>>,
-    ev_mousebtn: Res<Events<MouseButtonInput>>,
-    ev_scroll: Res<Events<MouseWheel>>,
+    ev_mouse_button: Res<Events<MouseButtonInput>>,
 ) {
     // Keyboard input.
     for ev in state.keys.iter(&ev_keys) {
@@ -107,30 +131,16 @@ fn track_input_events(
     }
 
     // Absolute cursor position (in window coordinates).
-    for ev in state.cursor.iter(&ev_cursor) {
-        log::info!("Cursor at: {}", ev.position);
-    }
-
-    // Relative mouse motion.
-    for ev in state.motion.iter(&ev_motion) {
-        log::info!("Mouse moved {} pixels", ev.delta);
+    if let Some(ev) = state.cursor.latest(&ev_cursor) {
+        mouse_position.0 = ev.position;
     }
 
     // Mouse buttons.
-    for ev in state.mousebtn.iter(&ev_mousebtn) {
+    for ev in state.mouse_button.iter(&ev_mouse_button) {
         if ev.state.is_pressed() {
             log::info!("Just pressed mouse button: {:?}", ev.button);
         } else {
             log::info!("Just released mouse button: {:?}", ev.button);
         }
-    }
-
-    // Scrolling.
-    for ev in state.scroll.iter(&ev_scroll) {
-        log::info!(
-            "Scrolled vertically by {} and horizontally by {}.",
-            ev.y,
-            ev.x
-        );
     }
 }
