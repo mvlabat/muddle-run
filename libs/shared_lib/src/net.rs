7d552e5b2c59325683dc43910d031e94600d1a4f
mvlabat <mvlabat@gmail.com>
2021-03-26 12:47:04 +0200

    Fix client input channel settings

--- libs/shared_lib/src/net.rs
+++ libs/shared_lib/src/net.rs
@@ -339,21 +339,7 @@ pub fn network_setup(mut net: ResMut<NetworkResource>) {
 
 const CLIENT_INPUT_MESSAGE_SETTINGS: MessageChannelSettings = MessageChannelSettings {
     channel: 0,
-    channel_mode: MessageChannelMode::Reliable {
-        reliability_settings: ReliableChannelSettings {
-            bandwidth: 4096,
-            recv_window_size: 1024,
-            send_window_size: 1024,
-            burst_bandwidth: 1024,
-            init_send: 512,
-            wakeup_time: Duration::from_millis(100),
-            initial_rtt: Duration::from_millis(200),
-            max_rtt: Duration::from_secs(2),
-            rtt_update_factor: 0.1,
-            rtt_resend_factor: 1.5,
-        },
-        max_message_len: 1024,
-    },
+    channel_mode: MessageChannelMode::Unreliable,
     message_buffer_size: 8,
     packet_buffer_size: 8,
 };
