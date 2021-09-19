d68b8207a972694a9f6bb76b656bd65dc40999da
mvlabat <mvlabat@gmail.com>
2021-09-19 16:29:13 +0300

    Fix stale collisions on updating level objects

--- libs/shared_lib/src/game/collisions.rs
+++ libs/shared_lib/src/game/collisions.rs
@@ -1,5 +1,5 @@
 use crate::game::{
-    components::{PlayerSensor, PlayerSensorState, PlayerSensors},
+    components::{LevelObjectTag, PlayerSensor, PlayerSensorState, PlayerSensors},
     events::CollisionLogicChanged,
     level::LevelParams,
 };
@@ -7,7 +7,7 @@ use bevy::{
     app::EventReader,
     ecs::{
         entity::Entity,
-        system::{In, Query, SystemParam},
+        system::{In, Query, RemovedComponents, SystemParam},
     },
     log,
     utils::HashSet,
@@ -29,31 +29,48 @@ pub fn process_collision_events(
     mut intersection_events: EventReader<IntersectionEvent>,
     mut collision_logic_changed_events: EventReader<CollisionLogicChanged>,
     mut queries: CollisionQueries,
+    removed_level_objects: RemovedComponents<LevelObjectTag>,
     level: LevelParams,
 ) -> Vec<Entity> {
     let mut changed_players = HashSet::default();
+    let removed_level_objects = removed_level_objects.iter().collect::<Vec<_>>();
 
-    for event in intersection_events.iter() {
-        let player_sensor = queries
-            .player_sensors
-            .get(event.collider1.entity())
-            .ok()
-            .or_else(|| queries.player_sensors.get(event.collider2.entity()).ok())
-            .map(|(entity, sensor)| (entity, sensor.clone()));
+    let mut all_events: Vec<(bool, Entity, Entity)> = Vec::new();
+    all_events.extend(intersection_events.iter().map(|event| {
+        (
+            event.intersecting,
+            event.collider1.entity(),
+            event.collider2.entity(),
+        )
+    }));
+    all_events.extend(contact_events.iter().map(|event| match event {
+        ContactEvent::Started(c1, c2) => (true, c1.entity(), c2.entity()),
+        ContactEvent::Stopped(c1, c2) => (false, c1.entity(), c2.entity()),
+    }));
 
-        let (level_object_entity, level_object) = match (
-            level.level_object_by_entity(event.collider1.entity()),
-            level.level_object_by_entity(event.collider2.entity()),
+    for (contacting, entity1, entity2) in all_events.into_iter() {
+        let (level_object_entity, level_object, other_entity) = match (
+            level.level_object_by_entity(entity1),
+            level.level_object_by_entity(entity2),
         ) {
-            (Some(level_object), None) => (event.collider1.entity(), level_object),
-            (None, Some(level_object)) => (event.collider2.entity(), level_object),
-            _ => continue,
+            (Some(level_object), None) => (entity1, level_object, entity2),
+            (None, Some(level_object)) => (entity2, level_object, entity1),
+            _ => {
+                log::error!(
+                    "None of the intersected entities is a level object: {:?}, {:?}",
+                    entity1,
+                    entity2
+                );
+                continue;
+            }
         };
 
-        if let Some((player_sensor_entity, PlayerSensor(player_entity))) = player_sensor {
+        if let Ok((player_sensor_entity, PlayerSensor(player_entity))) =
+            queries.player_sensors.get(other_entity)
+        {
             let mut player_sensors = queries
                 .players
-                .get_component_mut::<PlayerSensors>(player_entity)
+                .get_component_mut::<PlayerSensors>(*player_entity)
                 .expect("Expected a player for an existing sensor");
             let (_, sensor_state) = player_sensors
                 .sensors
@@ -61,7 +78,7 @@ pub fn process_collision_events(
                 .find(|(sensor_entity, _)| *sensor_entity == player_sensor_entity)
                 .expect("Player is expected to know a sensor connected to it");
 
-            if event.intersecting {
+            if contacting {
                 sensor_state
                     .contacting
                     .push((level_object_entity, level_object.collision_logic));
@@ -70,49 +87,15 @@ pub fn process_collision_events(
                     .contacting
                     .drain_filter(|(entity, _)| *entity == level_object_entity);
             }
-            changed_players.insert(player_entity);
-        } else if let Ok((_, mut player_sensors)) =
-            queries.players.get_mut(event.collider1.entity())
-        {
-            let player_entity = event.collider1.entity();
+            changed_players.insert(*player_entity);
+        } else if let Ok((_, mut player_sensors)) = queries.players.get_mut(other_entity) {
+            let player_entity = other_entity;
             // Intersection with a player collider itself.
-            if event.intersecting {
-                player_sensors
-                    .main
-                    .contacting
-                    .push((player_entity, level_object.collision_logic));
-            } else {
-                player_sensors
-                    .main
-                    .contacting
-                    .drain_filter(|(entity, _)| *entity == event.collider2.entity());
-            }
-            changed_players.insert(player_entity);
-        }
-    }
-
-    for event in contact_events.iter() {
-        let (contacting, entity1, entity2) = match event {
-            ContactEvent::Started(entity1, entity2) => (true, entity1.entity(), entity2.entity()),
-            ContactEvent::Stopped(entity1, entity2) => (false, entity1.entity(), entity2.entity()),
-        };
-
-        let (player_entity, level_object_entity, level_object) = match (
-            level.level_object_by_entity(entity1),
-            level.level_object_by_entity(entity2),
-        ) {
-            (Some(level_object), None) => (entity2, entity1, level_object),
-            (None, Some(level_object)) => (entity1, entity2, level_object),
-            _ => continue,
-        };
-
-        // We expect contact events to happen only to players.
-        if let Ok((_, mut player_sensors)) = queries.players.get_mut(player_entity) {
             if contacting {
                 player_sensors
                     .main
                     .contacting
-                    .push((player_entity, level_object.collision_logic));
+                    .push((level_object_entity, level_object.collision_logic));
             } else {
                 player_sensors
                     .main
@@ -124,26 +107,30 @@ pub fn process_collision_events(
     }
 
     let changed_collision_logic: Vec<_> = collision_logic_changed_events.iter().collect();
-    if !changed_collision_logic.is_empty() {
+    if !changed_collision_logic.is_empty() || !removed_level_objects.is_empty() {
         for (player_entity, mut player_sensors) in queries.players.iter_mut() {
-            for CollisionLogicChanged {
-                level_object_entity,
-                collision_logic,
-            } in changed_collision_logic.iter()
-            {
-                let mut update_collision_logic = |sensor_state: &mut PlayerSensorState| {
-                    for (contacted_entity, logic) in &mut sensor_state.contacting {
-                        if contacted_entity == level_object_entity {
-                            *logic = *collision_logic;
+            let mut update_collision_logic = |sensor_state: &mut PlayerSensorState| {
+                sensor_state
+                    .contacting
+                    .drain_filter(|(contacted_entity, logic)| {
+                        if removed_level_objects.contains(contacted_entity) {
+                            return true;
+                        }
+
+                        if let Some(changed) = changed_collision_logic
+                            .iter()
+                            .find(|changed| changed.level_object_entity == *contacted_entity)
+                        {
+                            *logic = changed.collision_logic;
                             changed_players.insert(player_entity);
                         }
-                    }
-                };
+                        false
+                    });
+            };
 
-                update_collision_logic(&mut player_sensors.main);
-                for (_, sensor) in &mut player_sensors.sensors {
-                    update_collision_logic(sensor);
-                }
+            update_collision_logic(&mut player_sensors.main);
+            for (_, sensor) in &mut player_sensors.sensors {
+                update_collision_logic(sensor);
             }
         }
     }
