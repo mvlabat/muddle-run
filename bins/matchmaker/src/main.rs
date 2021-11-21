718ab08b01266bd6bf2550faf3b314fd83c6e5c5
mvlabat <mvlabat@gmail.com>
2021-11-21 15:50:52 +0200

    Update matchmaker to read player stats from game server updates

--- bins/matchmaker/src/main.rs
+++ bins/matchmaker/src/main.rs
@@ -350,7 +350,7 @@ async fn handle_connection(
     mut rx: Receiver<MatchmakerMessage>,
     servers: Servers,
 ) {
-    log::info!("Incoming TCP connection from: {}", addr);
+    log::debug!("Incoming TCP connection from: {}", addr);
 
     let ws_stream = match tokio_tungstenite::accept_async(stream).await {
         Ok(ws_stream) => ws_stream,
@@ -453,8 +453,8 @@ fn server_command_from_resource(resource: &GameServer) -> Option<ServerCommand> 
             Some(ServerCommand::Update(Server {
                 name,
                 addr: SocketAddr::new(ip_addr, port),
-                player_capacity: PLAYER_CAPACITY,
-                player_count: 0,
+                player_capacity: status.players.capacity as u16,
+                player_count: status.players.count as u16,
             }))
         })
 }
