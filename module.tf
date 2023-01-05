6bbbc0cf033217c45d0740a4abb8930b2bf94755
mvlabat <mvlabat@gmail.com>
2023-01-06 00:56:02 +0200

    Update eks auth api_version to v1beta1

--- module.tf
+++ module.tf
@@ -86,7 +86,7 @@ provider "helm" {
     host                   = data.aws_eks_cluster.current.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.current.certificate_authority[0].data)
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
       command     = "aws"
     }
@@ -97,7 +97,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.current.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.current.certificate_authority[0].data)
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
     command     = "aws"
   }
